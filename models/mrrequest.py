f0f6cb3227af1e5b7988949a606bc1441fde07d3
Oak Soe San <pm@mingalarsky.com>
2024-03-24 17:34:22 +0630

    Restructure the model fields

--- models/mrrequest.py
+++ models/mrrequest.py
@@ -1,21 +1,53 @@
 from openerp import fields, models, api
-from openerp.exceptions import ValidationError
-from datetime import datetime, timedelta, date
-import time
-from openerp.tools.translate import _
 
-class MRrequest(models.Model):
+class MrRequest(models.Model):
     _name = 'mrrequest'
     _description = 'MR request model'
 
+    # Left Panel (User)
     name = fields.Char('Name')
-    mrdate = fields.Date('Date')
-    department = fields.Many2one("hr.department",string="Department",store=True)
-    location_id = fields.Many2one('stock.location',string="Location")
-    prob = fields.Char('Problem Detail')
-    priority = fields.Selection([('0','High'),('1','Medium'),('2','Low')],track_visibility='onchange')
-    action_plan = fields.Char('Action Plan For the Problem')
-    to_assign = fields.Selection([('0','Own Staff'),('1','Outside Contractor')],track_visibility='onchange')
-    Estimate_cost = fields.Char('Estimate Cost')
-    Estimate_time = fields.Char('Estimate Time')
-    
+    res_partner_id = fields.Many2one('res.partner', required=True,
+                                     string='Requested By')
+    department = fields.Many2one('hr.department', string="Department",
+                                 store=True)
+    priority = fields.Selection(
+        [
+            ('0', 'Low'),
+            ('1', 'Normal'),
+            ('2', 'High')
+        ],
+        'Priority'),
+    date_time = fields.Datetime(string='Request Date & Time', required=True)
+    mr_location_id = fields.Many2one('mrrequest.location', string='Location')
+    problem_detail = fields.Text('Problem Detail')
+
+    # Right Panel (MR Admin)
+    hr_employee_id = fields.Many2one('hr.employee', string='Assign To',
+                                     track_visibility='onchange')
+    estimate_cost = fields.Char('Estimated Cost')
+    estimate_start_time = fields.Datetime(string='Estimate Start at')
+    estimate_end_time = fields.Datetime(string='Estimate End at')
+    actual_start_time = fields.Datetime(string='Actual Start at')
+    actual_end_time = fields.Datetime(string='Actual End at')
+    job_status = fields.Selection([
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ])
+    maintenance_detail = fields.Text('Maintenance Detail')
+
+    # Receiving Panel
+    received_status = fields.Selection([
+        ('satisfied', 'Satisfied'),
+        ('unsatisfied', 'Unsatisfied')
+    ],
+        string='Received Status')
+    received_date_time = fields.Datetime(string='Received At')
+    received_note = fields.Text('Received Note')
+    approver_comment = fields.Text('Approver Comment')
+
+
+class MrLocation(models.Model):
+    _name = 'mrrequest.location'
+    _description = 'Locations for MR requests'
+    name = fields.Char(string='Location Name')
